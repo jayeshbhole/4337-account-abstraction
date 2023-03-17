d7a8e8fd3527d2468b5598fcbb0ba633f0b00f2b
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2023-03-17 19:50:19 +0530

    feat: deploy TestCounter

--- deploy/1_deploy_entrypoint.ts
+++ deploy/1_deploy_entrypoint.ts
@@ -16,24 +16,23 @@ const deployEntryPoint: DeployFunction = async function (hre: HardhatRuntimeEnvi
       deterministicDeployment: true
     })
   console.log('==entrypoint addr=', ret.address)
-/*
-  const entryPointAddress = ret.address
-  const w = await hre.deployments.deploy(
-    'SimpleAccount', {
-      from,
-      args: [entryPointAddress, from],
-      gasLimit: 2e6,
-      deterministicDeployment: true
-    })
 
-  console.log('== wallet=', w.address)
+  // const entryPointAddress = ret.address
+  // const w = await hre.deployments.deploy(
+  //   'SimpleAccount', {
+  //     from,
+  //     args: [entryPointAddress, from],
+  //     gasLimit: 2e6,
+  //     deterministicDeployment: true
+  //   })
+
+  // console.log('== wallet=', w.address)
 
   const t = await hre.deployments.deploy('TestCounter', {
     from,
     deterministicDeployment: true
   })
   console.log('==testCounter=', t.address)
-  */
 }
 
 export default deployEntryPoint
